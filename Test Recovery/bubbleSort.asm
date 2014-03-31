164a193dffe5d766d1a59094c3a6318bafea2f05
MGreathouse <marcus.greathouse@cs.olivetcollege.edu>
2014-03-31 10:17:50 -0400

    Bubble sort now seems to work
    never again....

--- Test Recovery/bubbleSort.asm
+++ Test Recovery/bubbleSort.asm
@@ -14,15 +14,15 @@
 ; Initialiation
         AND     R0, R0, 0   ; set R0 to 0
         ADD     R0, R0, #-1 ; set R0 to -1
-        LDI     R6, BASE    ; sets R6 to the base data value
-        LDI     R3, BASE    ; set R3 to base as well
+        LD      R6, BASE    ; sets R6 to the base data value
+        ADD     R3, R6, 0   ; set R3 to base as well
         ADD     R3, R3, #-1 ; sets R3 to 1 less
 ;
 ; Bubble Sort
 ; Find end
 NEXT    ADD     R0, R0, #1  ; increments counter by one
         ADD     R3, R3, #1  ; increments address by one
-        LDR      R1, R3, 0   ; puts next value into R1
+        LDR     R1, R3, 0   ; puts next value into R1
         BRnp    NEXT        ; runs until setinal value is found
 
         AND     R5, R5, 0   ; set R5 to 0
@@ -32,26 +32,22 @@ NEXT    ADD     R0, R0, #1  ; increments counter by one
 ;
 ; Sort
 SORT    ADD     R5, R5, #1  ; increment R5 by 1 ~ outer loop
-        BRz     ESCAPE      ; escape case
+        BRzp    ESCAPE      ; escape case
         AND     R0, R0, 0   ; reset R0
-        LDI     R3, BASE    ; reset R3
+        ADD     R3, R6, 0   ; reset R3
 LOOP    LDR     R1, R3, 0   ; loads current value to check ~ inner loop
-        ADD     R0, R0, #1  ; increments counter by one
-        ADD     R3, R3, #1  ; increments address by one
-        LDR     R2, R3, 0   ; load next value to check
+        LDR     R2, R3, #1  ; load next value to check
         NOT     R4, R2      ; invert R2 to check against
         ADD     R4, R4, #1  ;
         ADD     R4, R4, R1  ; check to see if current value is greater than next value
         BRp     SWAP        ; if positive swap the values
-PROCEED ;ADD     R0, R0, #-1 ; return R0 to initial increment
-        ;ADD     R3, R3, #-1 ; decrements address by one
-        ADD     R4, R0, R6  ; check to see if inner loop is done
+PROCEED ADD     R0, R0, #1  ; return R0 to initial increment
+        ADD     R3, R3, #1  ; decrements address by one
+        ADD     R4, R0, R5  ; check to see if inner loop is done
         BRz     SORT        ; if zero, go to the next level
         BRnzp   LOOP        ; next loop iteratio
 
-SWAP    ADD     R0, R0, #-1 ; go back a value so they can be swapped
-        ADD     R3, R3, #-1 ; deccrements address by one
-        STR     R2, R3, 0   ; store R2 where R1 was
+SWAP    STR     R2, R3, 0   ; store R2 where R1 was
         ADD     R3, R3, #1  ; increments address by one
         ADD     R0, R0, #1  ; increment counter
         STR     R1, R3, 0   ; store R1 where R2 was
